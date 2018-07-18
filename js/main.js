add4d8a510f01640cf0d6523640a5bea70ecbc5d
Chie Noda <chienoda119@gmail.com>
2018-07-18 14:19:56 -0400

    Initial commmit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var button = document.querySelector("button");
+var isPurple = false;
+
+button.addEventListener("click", function() {
+	//if white
+	 //make it purple
+	//else
+	 //make it white
+
+	if(isPurple) {
+		document.body.style.background = "white";
+		// isPurple = false;
+	} else {
+		document.body.style.background = "purple";
+		// isPurple = true;
+	}
+	isPurple = !isPurple;
+});
+
+
